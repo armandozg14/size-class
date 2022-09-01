56394921eefa859de76bb2aa8830b6f15cd9386f
analourdescr <ana_lourdes98@hotmail.com>
2022-09-01 07:23:13 -0700

    Initial Commit

---
+++ size-class/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  size-class
+//
+//  Created by Alumno on 9/1/22.
+//  Copyright © 2022 Alumno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
